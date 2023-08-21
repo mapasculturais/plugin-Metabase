5ad021102fee3a6ab3b1b624d0eed186574b9513
erleibiazzio <erleibiazzio@gmail.com>
2023-08-21 15:14:32 -0300

    Cria estrutura base do plugin Metabase

---
+++ Plugin.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Metabase;
+
+class Plugin extends \MapasCulturais\Plugin
+{
+    function __construct($config = [])
+    {
+        $config += [];
+
+        parent::__construct($config);
+    }
+
+    public function _init()
+    {
+    }
+
+    public function register()
+    {
+    }
+}
