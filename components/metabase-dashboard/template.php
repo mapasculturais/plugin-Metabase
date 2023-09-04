1b906e406873c13923f0a98f767e1708ad288634
robedj <ed.wassouf@gmail.com>
2023-09-04 10:04:43 -0400

    metabase-dashboard iteração para exibição  Ref.: #1 #2

--- components/metabase-dashboard/template.php
+++ components/metabase-dashboard/template.php
@@ -27,55 +27,6 @@ $base_url=$app->createUrl('metabase', 'dashboard');
                 <a :href="'<?=$base_url ?>?panelId='+name" class="button button--small button--primary"><?php i::_e("conferir painel") ?></a>
             </div>
         </div>
-        <!-- <div class="metabase-dashboard__card">
-            <h4 class="bold"><?php i::_e("Painel sobre oportunidades") ?></h4>
-            <p><?php i::_e("Tenha acesso ao número de oportunidades e  editais cadastrados, a quantidade de pessoas participantes inscritas, o perfil demográfico e mais informações.") ?> </p>
-            <div class="metabase-dashboard__btn">
-                <mc-link route="metabase/dashboard" class="button button--small button--primary"><?php i::_e("conferir painel") ?></mc-link>
-            </div>
-        </div>
-        <div class="metabase-dashboard__card">
-            <h4 class="bold"><?php i::_e("Painel sobre entidades em geral") ?></h4>
-            <p><?php i::_e("Confira dados relacionados às entidades cadastradas na plataforma, como agentes individuais e coletivos, oportunidades, espaços, eventos e projetos.") ?> </p>
-            <div class="metabase-dashboard__btn">
-                <mc-link route="metabase/dashboard" class="button button--small button--primary"><?php i::_e("conferir painel") ?></mc-link>
-            </div>
-        </div>
-        <div class="metabase-dashboard__card">
-            <h4 class="bold"><?php i::_e("Painel sobre agentes individuais") ?></h4>
-            <p><?php i::_e("Saiba os números de agentes individuais cadastrados, quantos são criados mensalmente, por onde estão distribuídos no território e outras informações.") ?> </p>
-            <div class="metabase-dashboard__btn">
-                <mc-link route="metabase/dashboard" class="button button--small button--primary"><?php i::_e("conferir painel") ?></mc-link>
-            </div>
-        </div>
-        <div class="metabase-dashboard__card">
-            <h4 class="bold"><?php i::_e("Painel sobre agentes coletivos") ?></h4>
-            <p><?php i::_e("Dados sobre a quantidade de  coletivos e instituições (com ou sem CNPJ) cadastrados, por onde se distribuem pelo estado e outras informações.") ?> </p>
-            <div class="metabase-dashboard__btn">
-                <mc-link route="metabase/dashboard" class="button button--small button--primary"><?php i::_e("conferir painel") ?></mc-link>
-            </div>
-        </div>
-        <div class="metabase-dashboard__card">
-            <h4 class="bold"><?php i::_e("Painel sobre espaços") ?></h4>
-            <p><?php i::_e("Conheça, entre outras informações, por onde os espaços estão distribuídos, a quantidade de espaços cadastros na plataforma, os tipos e as áreas de atuação.") ?> </p>
-            <div class="metabase-dashboard__btn">
-                <mc-link route="metabase/dashboard" class="button button--small button--primary"><?php i::_e("conferir painel") ?></mc-link>
-            </div>
-        </div>
-        <div class="metabase-dashboard__card">
-            <h4 class="bold"><?php i::_e("Painel sobre eventos") ?></h4>
-            <p><?php i::_e("Indicadores relacionados a quantidade de eventos cadastrados, às linguagens culturais e características, as datas de criação e também eventos agendados.") ?> </p>
-            <div class="metabase-dashboard__btn">
-                <mc-link route="metabase/dashboard" class="button button--small button--primary"><?php i::_e("conferir painel") ?></mc-link>
-            </div>
-        </div>
-        <div class="metabase-dashboard__card">
-            <h4 class="bold"><?php i::_e("Painel sobre projetos") ?></h4>
-            <p><?php i::_e("Tenah acesso ao número total de projetos cadastrados, projetos certificados, quantidade de projetos com subprojetos, os tipos e outros dados. ") ?> </p>
-            <div class="metabase-dashboard__btn">
-                <mc-link route="metabase/dashboard" class="button button--small button--primary"><?php i::_e("conferir painel") ?></mc-link>
-            </div>
-        </div> -->
     </div>
 
 </div>
