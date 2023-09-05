9872777077dd1265b838eb9ee3e23504a0c38852
robedj <ed.wassouf@gmail.com>
2023-09-05 16:45:08 -0400

    Refatoração do template e js do componente home-metabase Ref.: #1 #2

--- components/home-metabase/script.js
+++ components/home-metabase/script.js
@@ -14,110 +14,11 @@ app.component('home-metabase', {
         return { text, hasSlot }
     },
 
-    async created() {
-        const opportunityAPI = new API('opportunity');
-        const opportunityQuery = Utils.parsePseudoQuery(this.initialPseudoQuery);
-
-        await opportunityAPI.find(opportunityQuery, null, true).then(response => {
-            this.opportunity.registered = response.length;
-
-        });
-
-        opportunityQuery['@verified'] = 1;
-        await opportunityAPI.find(opportunityQuery, null, true).then(response => {
-            this.opportunity.verified = response.length;
-
-        });
-
-        const agentAPI = new API('agent');
-        const agentQuery = Utils.parsePseudoQuery(this.initialPseudoQuery);
-
-        agentQuery['type'] = 'EQ(1)';
-        await agentAPI.find(agentQuery, null, true).then(response => {
-            this.agents.individual.registered = response.length;
-
-        });
-
-        agentQuery['type'] = 'EQ(2)';
-        await agentAPI.find(agentQuery, null, true).then(response => {
-            this.agents.collective.registered = response.length;
-
-        });
-
-        agentQuery['@verified'] = 1;
-        await agentAPI.find(agentQuery, null, true).then(response => {
-            this.agents.collective.verified = response.length;
-
-        });
-
-        const spaceAPI = new API('space');
-        const spaceQuery = Utils.parsePseudoQuery(this.initialPseudoQuery);
-
-        await spaceAPI.find(spaceQuery, null, true).then(response => {
-            this.spaces.registered = response.length;
-
-        });
-
-
-        const projectAPI = new API('project');
-        const projectQuery = Utils.parsePseudoQuery(this.initialPseudoQuery);
-        projectQuery['@select'] = 'children';
-        await projectAPI.find(projectQuery, null, true).then(response => {
-            let projectChildren = 0;
-            for (project of response) {
-                if (project.children.length > 0) {
-                    projectChildren++;
-                }
-            }
-            this.projects.registered = response.length;
-            this.projects.subProjects = projectChildren;
-        });
-
-        const eventAPI = new API('event');
-        const eventQuery = Utils.parsePseudoQuery(this.initialPseudoQuery);
-
-        await eventAPI.find(eventQuery, null, true).then(response => {
-            this.events.registered = response.length;
-
-        });
-
-    },
-
-
     data() {
         return {
-           bigData: 77777777,
-            agents: {
-                individual: {
-                    registered: 0,
-                    verified: 0,
-                },
-                collective: {
-                    registered: 0,
-                    verified: 0,
-                },
-            },
-            spaces: {
-                registered: 0,
-                verified: 0,
-            },
-            projects: {
-                registered: 0,
-                verified: 0,
-                subProjects: 0,
-            },
-            opportunity: {
-                registered: 0,
-                verified: 0,
-            },
-            events: {
-                registered: 0,
-                verified: 0,
-            },
-            pseudoQuery: $MAPAS.initialPseudoQuery || {},
-
+            entities: $MAPAS.config.homeMetabase,
+            bigData: 1856974586
         }
-
     },
 
     computed: {
@@ -130,9 +31,7 @@ app.component('home-metabase', {
         lengthClass(text) {
             const textString = String(text);
             if (textString.length > 5) {
-                return 'home-metabase__long';
-            } else {
-                return 'home-metabase__short';
+                return 'metabase-card__number--long';
             }
         },
     },
--- components/home-metabase/template.php
+++ components/home-metabase/template.php
@@ -1,134 +1,45 @@
 <?php
-
 /**
  * @var MapasCulturais\App $app
  * @var MapasCulturais\Themes\BaseV2\Theme $this
  */
 
 use MapasCulturais\i;
-// $this->metabaseDataColect();
 $this->import('
     mc-link
     mc-icon
 ');
-
-
 ?>
 <div class="home-metabase">
     <div class="home-metabase__header">
         <div class="home-metabase__left">
             <h1 class="bold"><?php i::_e('Relatórios') ?></h1>
             <p class="semibold home-metabase__text"><?php i::_e('Acesse painéis de dados ao clicar no botão abaixo para visualizar gráficos e outras infomações importantes para consulta e análise') ?></p>
-            <a :href="panelURl" :class="['button', 'button--primary-noborder', {'button--large': $media('max-width: 500px')}]"><?php i::_e('Confira todos os painéis')?></a>
+            <a :href="panelURl" :class="['button', 'button--primary-noborder', {'button--large': $media('max-width: 500px')}]"><?php i::_e('Confira todos os painéis') ?></a>
 
         </div>
         <div class="home-metabase__right">
-            <img src="<?php $this->asset('img/business.png') ?>" class="home-metavase__img" />
+            <img src="<?php $this->asset('img/business.png') ?>" class="home-metabase__img" />
         </div>
     </div>
     <div class="home-metabase__panel">
         <h4 v-if="!$media('max-width: 500px')" class="semibold home-metabase__subtitle"><?php i::_e('Confira abaixo alguns dados destacados') ?></h4>
         <p v-if="$media('max-width: 500px')" class="semibold home-metabase__subtitle"><?php i::_e('Confira abaixo alguns dados destacados') ?></p>
         <div class="home-metabase__content">
-            <div class="home-metabase__card">
-                <div class="home-metabase__cardtitle">
-                    <mc-icon class="home-metabase__icon opportunity__color" name="opportunity"></mc-icon>
-                    <h4 class="bold"><?php i::_e('Oportunidades') ?></h4>
+            <div v-for="entity in entities" class="metabase-card" :class="{'metabase-card--double' : entity.data.length > 1}">
+                <div class="metabase-card__header">
+                    <mc-icon class="metabase-card__icon" :class="entity.iconClass" :name="entity.icon"></mc-icon>
+                    <h4 class="bold">{{entity.label}}</h4>
                 </div>
-                <div class="home-metabase__opcard">
-                    <div class="home-metabase__opinfo">
-                        <strong :class="[$media('max-width: 500px') ? 'bold home-metabase__lmvalue' : 'bold home-metabase__leftvalue', lengthClass(opportunity.registered) ]">{{opportunity.registered}}</strong>
-
-                        <label class="semibold home-metabase__leftlabel"><?php i::_e('oportunidades criadas') ?></label>
-                    </div>
-                    <div class="home-metabase__opinfo">
-
-                        <strong v-if="!$media('max-width: 500px')" class="bold home-metabase__rightvalue ">{{opportunity.verified}}</strong>
-                        <strong v-if="$media('max-width: 500px')" class="bold home-metabase__rightvalue">{{opportunity.verified}}</strong>
-                        <label class="semibold home-metabase__rightlabel"><?php i::_e('oportunidades
-                        certificadas') ?>
-                        </label>
-                    </div>
-                </div>
-                <button class="button button--primary button--large"><?php i::_e('Conferir painel de dados') ?></button>
-            </div>
-            <div class="home-metabase__card">
-                <div class="home-metabase__cardtitle">
-                    <mc-icon class="home-metabase__icon  agent__color" name="agent-1"></mc-icon>
-                    <h4 class="bold"><?php i::_e('Agentes individuais') ?></h4>
-                </div>
-                <div class="home-metabase__spcinfo">
-                    <strong class="bold home-metabase__value">{{agents.individual.registered}}</strong>
-                    <label class="semibold home-metabase__leftlabel"><?php i::_e('agentes individuais cadastrados') ?></label>
-                </div>
-                <button class="button button--primary button--large"><?php i::_e('Conferir painel de dados') ?></button>
-            </div>
-
-            <div class="home-metabase__card">
-                <div class="home-metabase__cardtitle">
-                    <mc-icon class="home-metabase__icon  agent__color" name="agent-2"></mc-icon>
-                    <h4 class="bold"><?php i::_e('Agentes coletivos') ?></h4>
-                </div>
-                <div class="home-metabase__opcard">
-                    <div class="home-metabase__opinfo">
-                        <strong :class="[$media('max-width: 500px') ? 'bold home-metabase__lmvalue' : 'bold home-metabase__leftvalue', 'lengthClass(agents.collective.registered)']">{{agents.collective.registered}}</strong>
-                        <label class="semibold home-metabase__label"><?php i::_e('coletivos cadastrados') ?></label>
-                    </div>
-                    <div class="home-metabase__opinfo">
-                        <strong :class="[$media('max-width: 500px') ? 'bold home-metabase__rmvalue' : 'bold home-metabase__rightvalue']">{{agents.collective.verified}}</strong>
-                        <label class="semibold home-metabase__leftlabel"><?php i::_e('coletivos certificados') ?>
-                        </label>
+                <div class="metabase-card__content">
+                    <div v-for="data in entity.data" class="metabase-card__info">
+                        <strong class="metabase-card__number" :class="lengthClass(data.value)">{{data.value}}</strong>
+                        <label class="metabase-card__label">{{data.label}}</label>
                     </div>
                 </div>
                 <button class="button button--primary button--large"><?php i::_e('Conferir painel de dados') ?></button>
             </div>
 
-            <div class="home-metabase__card">
-                <div class="home-metabase__cardtitle">
-                    <mc-icon class="home-metabase__icon space__color" name="space"></mc-icon>
-                    <h4 class="bold"><?php i::_e('Espaços') ?></h4>
-                </div>
-                <div class="home-metabase__spcinfo">
-                    <strong class="bold home-metabase__value">{{spaces.registered}}</strong>
-                    <label class="semibold home-metabase__leftlabel"><?php i::_e('espaços') ?></label>
-                </div>
-                <button class="button button--primary button--large"><?php i::_e('Conferir painel de dados') ?></button>
-            </div>
-
-            <div class="home-metabase__card">
-                <div class="home-metabase__cardtitle">
-                    <mc-icon class="home-metabase__icon project__color" name="project"></mc-icon>
-
-                    <h4 class="bold"><?php i::_e('Projetos') ?></h4>
-                </div>
-                <div class="home-metabase__opcard">
-                    <div class="home-metabase__opinfo">
-                        <strong v-if="!$media('max-width: 500px')" :class="['bold', 'home-metabase__leftvalue', lengthClass(projects.registered)]">{{projects.registered}}</strong>
-                        <h2 v-if="$media('max-width: 500px')" :class="['bold', 'home-metabase__valuem', lengthClass(projects.registered)]">{{projects.registered}}</h2>
-                        <label class="semibold home-metabase__label"><?php i::_e('projetos cadastrados') ?></label>
-                    </div>
-                    <div class="home-metabase__opinfo">
-
-                        <strong v-if="!$media('max-width: 500px')" class="bold home-metabase__rightvalue ">{{projects.subProjects}}</strong>
-                        <h3 v-if="$media('max-width: 500px')" class="bold">{{projects.subProjects}}</h3>
-                        <h5 class="semibold"><?php i::_e('projetos com subprojetos') ?></h5>   
-                    </div>
-                </div>
-                <button class="button button--primary button--large"><?php i::_e('Conferir painel de dados') ?></button>
-            </div>
-
-
-            <div class="home-metabase__card">
-                <div class="home-metabase__cardtitle">
-                    <mc-icon class="home-metabase__icon event__color" name="event"></mc-icon>
-                    <h4 class="bold"><?php i::_e('Eventos') ?></h4>
-                </div>
-                <div class="home-metabase__spcinfo">
-                    <strong class="bold home-metabase__value">{{events.registered}}</strong>
-                    <label class="semibold home-metabase__leftlabel"><?php i::_e('eventos') ?></label>
-                </div>
-                <button class="button button--primary button--large"><?php i::_e('Conferir painel de dados') ?></button>
-            </div>
         </div>
     </div>
 
