e14b5e93f747b386d3ebeeb6f18d789ce72bf49b
robedj <ed.wassouf@gmail.com>
2023-09-06 09:58:27 -0400

    Refatoração do template do componente home-metabase Ref.: #1 #2

--- components/home-metabase/template.php
+++ components/home-metabase/template.php
@@ -9,6 +9,7 @@ $this->import('
     mc-link
     mc-icon
 ');
+
 ?>
 <div class="home-metabase">
     <div class="home-metabase__header">
@@ -26,18 +27,18 @@ $this->import('
         <h4 v-if="!$media('max-width: 500px')" class="semibold home-metabase__subtitle"><?php i::_e('Confira abaixo alguns dados destacados') ?></h4>
         <p v-if="$media('max-width: 500px')" class="semibold home-metabase__subtitle"><?php i::_e('Confira abaixo alguns dados destacados') ?></p>
         <div class="home-metabase__content">
-            <div v-for="entity in entities" class="metabase-card" :class="{'metabase-card--double' : entity.data.length > 1}">
+            <div v-for="card in cards" class="metabase-card" :class="{'metabase-card--double' : card.data.length > 1}">
                 <div class="metabase-card__header">
-                    <mc-icon class="metabase-card__icon" :class="entity.iconClass" :name="entity.icon"></mc-icon>
-                    <h4 class="bold">{{entity.label}}</h4>
+                    <mc-icon class="metabase-card__icon" :class="card.iconClass" :name="card.icon"></mc-icon>
+                    <h4 class="bold">{{card.label}}</h4>
                 </div>
                 <div class="metabase-card__content">
-                    <div v-for="data in entity.data" class="metabase-card__info">
+                    <div v-for="data in card.data" class="metabase-card__info">
                         <strong class="metabase-card__number" :class="lengthClass(data.value)">{{data.value}}</strong>
                         <label class="metabase-card__label">{{data.label}}</label>
                     </div>
                 </div>
-                <button class="button button--primary button--large"><?php i::_e('Conferir painel de dados') ?></button>
+                <a :href="getUrl(card)" class="button button--primary button--large"><?php i::_e('Conferir painel de dados') ?></a>
             </div>
 
         </div>
